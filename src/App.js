3e4387a103453c0f973087b50efc1ae587f4ab3b
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-11-18 19:39:01 -0400

    Redirect in V6
    If you want to redirect a route in V6 you need to use:
    - Navigate component.
    - for server side rendering is different you must see the documentation of redirect on react router 6.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-  import React from "react";
-  import { Routes, Route } from "react-router-dom";
-  import {
-    Home,
-    About,
-    Events,
-    Products,
-    Contact,
-    Whoops404,
-    Services,
-    Location,
-    History,
-  } from "./pages";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import {
+  Home,
+  About,
+  Events,
+  Products,
+  Contact,
+  Whoops404,
+  Services,
+  Location,
+  History,
+} from "./pages";
 
-  export default function App() {
-    return (
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />}>
-            <Route path="services" element={<Services />} />
-            <Route path="history" element={<History />} />
-            <Route path="location" element={<Location />} />
-          </Route>
-          <Route path="/events" element={<Events />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Whoops404 />} />
-        </Routes>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="services" element={<Services />} />
+          <Route path="history" element={<History />} />
+          <Route path="location" element={<Location />} />
+        </Route>
+        <Route path="/events" element={<Events />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Whoops404 />} />
+        <Route
+          path="services"
+          element={<Navigate replace to="/about/services" />}
+        />
+      </Routes>
+    </div>
+  );
+}
